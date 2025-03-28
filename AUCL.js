5a2e63117906042aa616aeec98f84411f1dc24b9
Moh Rezaee <52729468+Moh-Rezaee@users.noreply.github.com>
2025-03-28 14:07:18 +1100

    Update AUCL.js

--- AUCL.js
+++ AUCL.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("https://raw.githubusercontent.com/Moh-Rezaee/AUCL/refs/heads/main/index.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("content-area").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading content:", error));
+    document.getElementById("body").innerHTML = "Hello world";
 });
